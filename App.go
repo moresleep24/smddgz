b65573ffb70abe3f10be99897df8a2653c22be4a
smddgz <smddgz@outlook.com>
2025-04-14 04:07:47 +0800

    init

---
+++ App.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"embed"
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"net/http"
+)
+
+//go:embed templates/*
+var templates embed.FS
+
+func main() {
+	r := gin.Default()
+	fs, _ := template.ParseFS(templates, "templates/*")
+	r.SetHTMLTemplate(fs)
+	r.GET("/", func(c *gin.Context) {
+		// 返回 JSON 格式的响应
+		c.HTML(http.StatusOK,"home.html",gin.H{
+			"msg":"Hello",
+		})
+	})
+	r.Run(":8080")
+}
+
+
