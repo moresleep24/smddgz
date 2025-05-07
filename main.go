b22ad7c40091a41631e9329ff3bdb7d1b3d432f0
Administrator <q@q.com>
2025-05-08 01:44:16 +0800

    add

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"html/template"
 	"net/http"
+	"smddgz/core"
 )
 
 //go:embed templates/*
@@ -14,15 +15,15 @@ func main() {
 	r := gin.Default()
 	fs, _ := template.ParseFS(templates, "templates/*")
 	r.SetHTMLTemplate(fs)
-	r.GET("/", func(c *gin.Context) {
-		// 返回 JSON 格式的响应
-		c.HTML(http.StatusOK, "home.html", gin.H{
-			"msg": "Hello",
-		})
-	})
-	//r.GET("/vmess", func(c *gin.Context) {
-	//	// 返回 JSON 格式的响应
-	//	c.String(http.StatusOK, GetLink())
-	//})
+	r.GET("/", home)
+	r.GET("/vmess", vmess)
 	r.Run(":80")
+}
+
+func home(c *gin.Context) {
+	c.String(http.StatusOK, "Hello World")
+}
+
+func vmess(c *gin.Context) {
+	c.String(http.StatusOK, core.GetLink())
 }
